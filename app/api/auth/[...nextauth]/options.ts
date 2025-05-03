3509001126431d669dfc8a7e5da4452fdb7af098
HaseoTheTerrorOfDeath <lucasfelicianodesouza51@gmail.com>
2025-05-03 14:27:03 -0300

    Pão de Alho de Bacon

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,20 @@
+import GoogleProvider from 'next-auth/providers/google'
+import type { NextAuthOptions } from 'next-auth'
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  pages: {
+    signIn: '/', // página inicial personalizada
+  },
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      return '/contabilidade/movimentacoes'
+    },
+  },
+}
+
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,25 +1,6 @@
 import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
-import type { NextAuthOptions } from 'next-auth'
+import { authOptions } from './options'
 
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  pages: {
-    signIn: '/', // Página inicial personalizada (opcional)
-  },
-  callbacks: {
-    async redirect({ url, baseUrl }) {
-      return '/contabilidade/movimentacoes' // Redireciona após login com sucesso
-    },
-  },
-}
-
-// Handler para API Route do App Router
 const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
